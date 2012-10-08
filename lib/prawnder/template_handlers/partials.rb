1a7da0805a2fbb9ac4ec965982013e46cb6e832a
Dave McNelis <davemcnelis@market76.com>
2012-10-08 12:28:40 -0400

    Partials
    Add options to render
    Add accessors to Prawn and PrawnChart DSL's

--- lib/prawnder/template_handlers/partials.rb
+++ lib/prawnder/template_handlers/partials.rb
@@ -2,13 +2,13 @@ module Prawnder
   module TemplateHandlers
     module Partials
 
-      # Can be called within a prawn template to render a partial below it.
+      # Can be called within a prawn template to render a partial in place.
       # :partial_name - Current has to be the entire path from the views folder. Doesn't recognize the folder of the
       #                 current template.
       # :prawn_object - The object to use for the pdf object in the partial.
       #                 Defaults to the pdf document, but can take a paramenter to render within a prawn object. This
       #                 is good for items like tables, text_blocks, etc.
-      def partial!(partial_name, prawn_object = pdf)
+      def render(partial_name, prawn_object = pdf,*options)
         @pdf_stack ||= []
         @pdf_stack.push @pdf
         @pdf = prawn_object
