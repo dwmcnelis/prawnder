791276ef80c3377ec9ce33f061a509a81b6ed943
Dave McNelis <davemcnelis@market76.com>
2013-01-30 18:47:07 -0500

    Support for abstract controllers

--- lib/prawnder/compile_support.rb
+++ lib/prawnder/compile_support.rb
@@ -20,12 +20,14 @@ module Prawnder
     #--------------------------------------------------------------------------------------------------#
 
     def set_headers
-      if not called_from_view_spec? and not called_from_mailer?
-        set_pragma
-        set_cache_control
-        set_content_type
-        set_disposition
-        set_other_headers_for_ie_ssl
+      if @controller.respond_to?(:headers)
+        if not called_from_view_spec? and not called_from_mailer?
+          set_pragma
+          set_cache_control
+          set_content_type
+          set_disposition
+          set_other_headers_for_ie_ssl
+        end
       end
     end
 
@@ -44,33 +46,36 @@ module Prawnder
     #--------------------------------------------------------------------------------------------------#
 
     def ie_request?
-      @controller.request.env['HTTP_USER_AGENT'] =~ /msie/i
+      @controller.respond_to?(:request) ? @controller.request.env['HTTP_USER_AGENT'] =~ /msie/i : false
     end
 
     #--------------------------------------------------------------------------------------------------#
 
     def ssl_request?
-      @controller.request.ssl?
+      @controller.respond_to?(:request) ? @controller.request.ssl? : false
     end
 
     #--------------------------------------------------------------------------------------------------#
 
     def set_other_headers_for_ie_ssl
-      return unless ssl_request? && ie_request?
-
-      @controller.headers['Content-Description'] = 'File Transfer'
-      @controller.headers['Content-Transfer-Encoding'] = 'binary'
-      @controller.headers['Expires'] = '0'
+      if @controller.respond_to?(:headers)
+        return unless ssl_request? && ie_request?
+        @controller.headers['Content-Description'] = 'File Transfer'
+        @controller.headers['Content-Transfer-Encoding'] = 'binary'
+        @controller.headers['Expires'] = '0'
+      end
     end
 
     #--------------------------------------------------------------------------------------------------#
 
     # TODO: kept around from railspdf-- maybe not needed anymore? should check.
     def set_pragma
-      if ssl_request? && ie_request?
-        @controller.headers['Pragma'] = 'public' # added to make ie ssl pdfs work (per naisayer)
-      else
-        @controller.headers['Pragma'] ||= ie_request? ? 'no-cache' : ''
+      if @controller.respond_to?(:headers)
+        if ssl_request? && ie_request?
+          @controller.headers['Pragma'] = 'public' # added to make ie ssl pdfs work (per naisayer)
+        else
+          @controller.headers['Pragma'] ||= ie_request? ? 'no-cache' : ''
+        end
       end
     end
 
@@ -78,25 +83,31 @@ module Prawnder
 
     # TODO: kept around from railspdf-- maybe not needed anymore? should check.
     def set_cache_control
-      if ssl_request? && ie_request?
-        @controller.headers['Cache-Control'] = 'maxage=1' # added to make ie ssl pdfs work (per naisayer)
-      else
-        @controller.headers['Cache-Control'] ||= ie_request? ? 'no-cache, must-revalidate' : ''
+      if @controller.respond_to?(:headers)
+        if ssl_request? && ie_request?
+          @controller.headers['Cache-Control'] = 'maxage=1' # added to make ie ssl pdfs work (per naisayer)
+        else
+          @controller.headers['Cache-Control'] ||= ie_request? ? 'no-cache, must-revalidate' : ''
+        end
       end
     end
 
     #--------------------------------------------------------------------------------------------------#
 
     def set_content_type
-      @controller.response.content_type ||= Mime::PDF
+      if @controller.respond_to?(:headers)
+        @controller.response.content_type ||= Mime::PDF
+      end
     end
 
     #--------------------------------------------------------------------------------------------------#
 
     def set_disposition
-      inline = options[:inline] ? 'inline' : 'attachment'
-      filename = options[:filename] ? "filename=\"#{options[:filename]}\"" : nil
-      @controller.headers["Content-Disposition"] = [inline,filename].compact.join(';')
+      if @controller.respond_to?(:headers)
+        inline = options[:inline] ? 'inline' : 'attachment'
+        filename = options[:filename] ? "filename=\"#{options[:filename]}\"" : nil
+        @controller.headers["Content-Disposition"] = [inline,filename].compact.join(';')
+      end
     end
 
   end
